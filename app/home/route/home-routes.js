0222e805f2a74801283c417593904faee4ff3ca4
sarwin-github <dapito.sherwin@yahoo.com>
2018-07-31 12:09:03 +0800

    updated packages

--- app/home/route/home-routes.js
+++ app/home/route/home-routes.js
@@ -3,7 +3,6 @@ const router    = express();
 
 const homeController = require('../controller/home-controller');
 
-/* Get The home page with list of fitness option */
 router.route('/').get(homeController.getHome);
 
 module.exports = router;
